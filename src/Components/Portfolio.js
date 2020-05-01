62b9da50e5783bc808f4f53e2ea8d7e04e0ad3a3
Imron Hajiahmad <imronha@gmail.com>
2020-05-01 14:41:14 -0700

    fixed contact form media queries

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -16,10 +16,10 @@ class Portfolio extends Component {
                     <p>{projects.category}</p>
                   </div>
                 </div>
-                <div className="link-icon">
-                  <i className="fa fa-link"></i>
-                </div>
               </a>
+              <div className="link-icon">
+                <i className="fa fa-chain"></i>
+              </div>
             </div>
           </div>
         );
