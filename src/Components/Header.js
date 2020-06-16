dff62cbd4492e95523fb244c90e6ae3fc096a30d
Imron Hajiahmad <imronha@gmail.com>
2020-06-16 10:24:48 -0700

    description updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -67,8 +67,9 @@ class Header extends Component {
           <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
             <h3>
-              I'm a {city} based aspiring <span>{occupation}</span> by day and
-              <span> hacker </span>by night. {description}.
+              {/* I'm a {city} based aspiring <span>{occupation}</span> by day and
+              <span> hacker </span>by night. {description}. */}
+              {description}
             </h3>
             <hr />
             <ul className="social">{networks}</ul>
